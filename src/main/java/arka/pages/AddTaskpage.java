9a8796da5378d7671e98dcc88e660bea8f0a2994
Ritika <ritika.kanchan1@gmail.com>
2023-10-04 19:32:09 +0530

    updated wait

--- src/main/java/arka/pages/AddTaskpage.java
+++ src/main/java/arka/pages/AddTaskpage.java
@@ -57,6 +57,7 @@ public class AddTaskpage {
 		String filePath= path+"/TestData/demo.png";
 		System.out.println(filePath);
 		//Utility.waitForWebElement(driver, selectFile).sendKeys(filePath);
+		Utility.wait(2);
 		driver.findElement(selectFile).sendKeys(filePath);
 		Utility.waitForWebElement(driver, uploadSaveBtn).click();
 		Utility.waitForWebElement(driver, uploadDelBtn).isDisplayed();
